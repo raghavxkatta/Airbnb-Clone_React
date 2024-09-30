19401495ce3905631d68bcc0ebd2471ec683b548
Raghav Katta <Raghavkatta2005@gmail.com>
2024-09-30 09:01:31 +0530

    Finall card component

--- airbnb-clone/src/components/Card.js
+++ airbnb-clone/src/components/Card.js
@@ -4,10 +4,11 @@ import Star from "../assets/Star.png"
 export default function Card(){
     return(
     <div className="card">
-<img src={Katie} alt="Katie" className="card--image"/>
+<img src={Katie} alt="Main card image." className="card--image"/>
 <div className="card--stats">
-<img src={Star} alt="Star" className="card--star"/>
-<span className="gray">5.0(6) • USA</span>
+<img src={Star} alt="Star icon." className="card--star"/>
+<span>5.0</span>
+<span className="gray">(6) • USA</span>
 </div>
 <h2>Life lessons with Katie Zaferes</h2>
 <p><span className="bold">From $136</span>/ person</p>
