694e41ed25026e6448552ffef09cb9724749abab
Raghav Katta <Raghavkatta2005@gmail.com>
2024-09-30 08:33:43 +0530

    card component

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import Card from './components/Card';
 function App() {
   return (
     <div className="App">
     <Navbar/>
     <Hero/>
+    <Card/>
           </div>
   );
 }
---
+++ airbnb-clone/src/components/Card.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Katie from "../assets/katie.png"
+import Star from "../assets/Star.png"
+export default function Card(){
+    <div className="card">
+<img src={Katie} alt="Katie" className="card--image"/>
+<div className="card--stats">
+<img src={Star} alt="Star" className="Star"/>
+<span className="gray">5.0(6) • USA</span>
+</div>
+<p className="star--text2">Life lessons with Katie Zaferes</p>
+<p><span className="bold">From $136</span>/ person</p>
+    </div>
+}
