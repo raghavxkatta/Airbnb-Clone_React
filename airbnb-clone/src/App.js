cbf09047363c20d8283817883b2f1d757a2613c7
Raghav Katta <Raghavkatta2005@gmail.com>
2024-10-07 11:56:03 +0530

    added data.js

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -2,12 +2,20 @@ import './App.css';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Card from './components/Card';
+import Katie from "./assets/katie.png"
 function App() {
   return (
     <div className="App">
     <Navbar/>
     <Hero/>
-    <Card/>
+    {/* so now we have a reusable component that when we use everytime I create an instance of my card, I pass in these props. I should be able to get different cards showing up on my list */}
+    <Card
+    img={Katie}
+    rating ="5.0"
+    reviewCount= {6}
+    country="USA"
+    title="Life Lessons with Katie Zaferes"
+    price={136}/>
           </div>
   );
 }
--- airbnb-clone/src/components/Card.js
+++ airbnb-clone/src/components/Card.js
@@ -1,17 +1,17 @@
 import React from "react"
-import Katie from "../assets/katie.png"
 import Star from "../assets/Star.png"
-export default function Card(){
+export default function Card(props){
+console.log(props)
     return(
     <div className="card">
-<img src={Katie} alt="Main card image." className="card--image"/>
+<img src={props.img} alt="Main card image." className="card--image"/>
 <div className="card--stats">
 <img src={Star} alt="Star icon." className="card--star"/>
-<span>5.0</span>
-<span className="gray">(6) • USA</span>
+<span>{props.reviewCount}</span>
+<span className="gray">(6) • {props.country}</span>
 </div>
-<h2>Life lessons with Katie Zaferes</h2>
-<p><span className="bold">From $136</span>/ person</p>
+<h2>{props.title}</h2>
+<p><span className="bold">From ${props.price}</span>/ person</p>
     </div>
     )
 }
