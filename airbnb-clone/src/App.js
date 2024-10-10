937d61666e4eeb3ca5dffa8b7ec52adad0ee778a
Raghav Katta <Raghavkatta2005@gmail.com>
2024-10-11 03:49:22 +0530

    final-images error fixed

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -5,15 +5,20 @@ import Card from './components/Card';
 import data  from './data'; 
 console.log(data  )
 function App() {
-  const cards=data.map(item=>{
-return(
+  const cards=data.map(item=>(
   <Card
   key={item.id} /* this is necessary to give every item should have one unique identity otherwise it would show error */
     /* cleaned all the properties from here and defined them in Card.js through data.js */
-    {...item}/* Spread syntax, with it we would not have to write props.item we can remove item as it would differentiate all the properties on it's own with the help of spread syntax. */
-/>
-)
-  })
+     title={item.title}
+    description={item.description}
+    price={item.price}
+    coverImg={item.coverImg}
+    stats={item.stats}
+    location={item.location}
+    openSpots={item.openSpots}
+    />
+))
+  
   return (
     <div className="App">
     <Navbar/>
--- airbnb-clone/src/components/Card.js
+++ airbnb-clone/src/components/Card.js
@@ -5,23 +5,28 @@ export default function Card(props){
 
     /* we have written item everywhere alongside props as when we did console.log(props) all the properties were coming alongside item so therefore props.item.propertyname*/
     let badgeText
-    if(props.item.openSpots===0){
+    if(props.openSpots===0){
         badgeText="SOLD OUT"
     }
-    else if(props.item.location==="Online"){
+    else if(props.location==="Online"){
         badgeText="ONLINE"
     }
     return(
     <div className="card">
        {/* so basically if badgetext exists then only the badge text would be displayed, basically if the first statement returns a truthy then only the following statement executes */}
         {badgeText && <div className="card--badge">{badgeText}</div>}
-<img src={`../assets/${props.item.coverImg}`} alt="Main card image." className="card--image"/>
+        <img 
+            src={require(`../assets/${props.coverImg}`)} 
+            alt="Main card image." 
+            className="card--image"
+        />
 <div className="card--stats">
 <img src={Star} alt="Star icon." className="card--star"/>
-<span className="gray">({props.item.stats.reviewCount}) • </span>
-<span className="gray">{props.item.location}</span>
+<span>{props.stats.rating}</span>
+<span className="gray">({props.stats.reviewCount}) • </span>
+<span className="gray">{props.location}</span>
 </div>
-<p className="card--title">{props.item.title}</p>
+<p className="card--title">{props.title}</p>
 <p className="card--price"><span className="bold">From ${props.price}</span>/ person</p>
     </div>
     )
