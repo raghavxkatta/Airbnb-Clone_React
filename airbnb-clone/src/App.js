975e624972c587259b9aea9b179553bd4d194f35
Raghav Katta <Raghavkatta2005@gmail.com>
2024-10-10 15:46:51 +0530

    final

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -10,7 +10,7 @@ return(
   <Card
   key={item.id} /* this is necessary to give every item should have one unique identity otherwise it would show error */
     /* cleaned all the properties from here and defined them in Card.js through data.js */
-    {...item}/* Spread syntax, with it we would not have to write props.item we can remove item as it would differentiate all the properties on it's own with the help of spread syntax */
+    {...item}/* Spread syntax, with it we would not have to write props.item we can remove item as it would differentiate all the properties on it's own with the help of spread syntax. */
 />
 )
   })
