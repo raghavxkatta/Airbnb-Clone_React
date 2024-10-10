c938c0838544655d221298b116a7f1e786960f97
Raghav Katta <Raghavkatta2005@gmail.com>
2024-10-10 09:19:51 +0530

    added styling for the sold out badge

--- airbnb-clone/src/components/Card.js
+++ airbnb-clone/src/components/Card.js
@@ -1,9 +1,17 @@
 import React from "react"
 import Star from "../assets/star.png"
 export default function Card(props){
-console.log(props.openSpots)
+    let badgeText
+    if(props.openSpots===0){
+        badgeText="SOLD OUT"
+    }
+    else if(props.location==="Online"){
+        badgeText="ONLINE"
+    }
     return(
     <div className="card">
+       {/* so basically if badgetext exists then only the badge text would be displayed, basically if the first statement returns a truthy then only the following statement executes */}
+        {badgeText && <div className="card--badge">{badgeText}</div>}
 <img src={`../assets/${props.img}`} alt="Main card image." className="card--image"/>
 <div className="card--stats">
 <img src={Star} alt="Star icon." className="card--star"/>
